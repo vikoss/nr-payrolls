4d532ca8bc74ff173459a4e39ff5eeb27fefa6b9
vikoss <vikoss.yiss@gmail.com>
2024-05-22 16:14:36 -0600

    feat: add payroll index view

---
+++ pages/nominas/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <v-select
+      v-model="year"
+      label="Año"
+      :items="['2021', '2022', '2023', '2024']"
+    ></v-select>
+    <v-btn
+      block
+      @click="getListPayrollByYear(year)"
+      :disabled="!year || loading"
+    >
+      Buscar
+    </v-btn>
+    <p class="mt-12 text-center" v-show="isCompleted && !listPayroll.length">
+      <span class="mdi mdi-alert-circle-outline"></span>
+      No tienes registro de nómina
+    </p>
+    <v-card
+      v-for="(payroll, index) in listPayroll"
+      :key="index"
+      :title="year"
+      :subtitle="payroll.fileName"
+      class="my-4"
+      elevation="2"
+    >
+      <v-card-actions>
+        <v-btn
+          elevation="2"
+          block
+          :disabled="loading"
+          @click="downloadPayroll(payroll.path)"
+        >
+          Descargar recibo de nómina
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <loading :show="loading" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { usePayrollManager } from '@/composables/usePayrollManager'
+import Loading from '@/components/Loading.vue'
+
+export default {
+  components: {
+    Loading,
+  },
+  setup() {
+    const {
+      getListPayrollByYear,
+      downloadPayroll,
+      listPayroll,
+      isCompleted,
+      loading,
+      error,
+    } = usePayrollManager()
+    const year = ref('')
+
+    return {
+      year,
+      getListPayrollByYear,
+      listPayroll,
+      downloadPayroll,
+      loading,
+      error,
+      isCompleted,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
