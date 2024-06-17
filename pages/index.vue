5481e37dce75cb31811392d6e7f2ec30b68e3f31
vikoss <vikoss.yiss@gmail.com>
2024-06-17 15:46:36 -0600

    feat: add card of change password to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex flex-col sm:flex-row gap-7">
     <v-card
       max-width="344"
       elevation="2"
@@ -13,7 +13,24 @@
       </v-card-subtitle>
       <v-card-actions>
         <v-btn elevation="2" @click="$router.push('/nominas')">
-          Consultar nómina
+          Ir a consultar nómina
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      max-width="344"
+      elevation="2"
+      prepend-icon="mdi-account-file-text-outline"
+    >
+      <template v-slot:title>
+        Contraseña
+      </template>
+      <v-card-subtitle>
+        Cambiar la contraseña de tu cuenta
+      </v-card-subtitle>
+      <v-card-actions>
+        <v-btn elevation="2" @click="$router.push('/autenticacion/contrasena')">
+          Ir a cambiar contraseña
         </v-btn>
       </v-card-actions>
     </v-card>
